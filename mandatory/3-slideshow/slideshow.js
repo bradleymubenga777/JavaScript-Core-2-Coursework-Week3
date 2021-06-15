87b56e122023532b66641bd7697cd0f7e07f1cc1
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-06-15 14:25:45 +0200

    Mandatory 3

--- mandatory/3-slideshow/slideshow.js
+++ mandatory/3-slideshow/slideshow.js
@@ -4,16 +4,16 @@ let fwdBtn = document.getElementById('forwardButton');
 let bckwdBtn = document.getElementById('backwardButton');
 
 //Storing Image Links As An Array
-let imageArray = ["https://images.unsplash.com/photo-1601758066440-cbfc06a82914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1550414485-9f22b971dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1534803359379-964dadf6c290?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1606509779095-de8fd5187049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1486530555807-11f29d0dff36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"]
+let imageArray = ["https://images.unsplash.com/photo-1601758066440-cbfc06a82914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80", "https://images.unsplash.com/photo-1609156770484-794ca6a3498a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"]
 
 // Creating An Event Listener For The Forward Button
 fwdBtn.addEventListener('click', (e) => {
     //
     e.preventDefault();
-    
-    //
-    let theIndex = Math.floor(Math.random(0 * 5));
 
     //
-    slideImage.setAttribute('src', imageArray[theIndex]);
+    for (i = 0; i < imageArray.length; i++) {
+        //
+        slideImage.setAttribute('src', imageArray[i]);
+    }
 })
